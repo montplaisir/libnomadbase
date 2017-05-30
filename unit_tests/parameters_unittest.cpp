b98255fc271116cd69c21317c26bd9e0464abbd8
Viviane Rochon Montplaisir <viviane.rochon.montplaisir@gerad.ca>
2017-05-30 16:48:03 -0400

    Unit tests for Parameters. Work in progress.

--- unit_tests/parameters_unittest.cpp
+++ unit_tests/parameters_unittest.cpp
@@ -17,6 +17,27 @@ TEST(ParametersTest, Basic) {
     // This test is named "Basic", and belongs to the "ParametersTest"
     // test case.
 
+    Parameters all_parameters = Parameters();
+    Param p1("first_parameter", 1.1);
+    bool inserted = all_parameters.add(p1);
+    //cout << "Param inserted: " << inserted << endl;
+
+    // Read parameters from a file
+    //all_parameters.read_from_file("mads_3.8.Dev.txt");
+    //cerr << "Exception thrown: " << e.what() << endl;
+
+/*
+    // Output parameters to a file
+    try
+    {
+        all_parameters.write_to_file("");
+    }
+    catch ( std::exception & e )
+    {
+        cerr << "Exception thrown: " << e.what() << endl;
+    }
+*/
+
 
 }
 
